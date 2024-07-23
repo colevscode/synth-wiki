ed2fd419f4c01a2d6595a6257b4a1fd78c928dba
Cole Krumbholz <cole@colevscode.io>
2024-07-23 16:26:08 -0700

    updating digitalocean docker script

--- dev/packer/scripts/010-docker.sh
+++ dev/packer/scripts/010-docker.sh
@@ -13,5 +13,5 @@ mkdir -p /etc/wiki
 docker network create wikinet
 docker volume create pgdata
 docker create --name=db -e POSTGRES_DB=wiki -e POSTGRES_USER=wiki -e POSTGRES_PASSWORD_FILE=/etc/wiki/.db-secret -v /etc/wiki/.db-secret:/etc/wiki/.db-secret:ro -v pgdata:/var/lib/postgresql/data --restart=unless-stopped -h db --network=wikinet postgres:11
-docker create --name=wiki -e DB_TYPE=postgres -e DB_HOST=db -e DB_PORT=5432 -e DB_PASS_FILE=/etc/wiki/.db-secret -v /etc/wiki/.db-secret:/etc/wiki/.db-secret:ro -e DB_USER=wiki -e DB_NAME=wiki -e UPGRADE_COMPANION=1 --restart=unless-stopped -h wiki --network=wikinet -p 80:3000 -p 443:3443 ghcr.io/colevscode/synth-wiki:2
+docker create --name=wiki -e LETSENCRYPT_DOMAIN=synth.guide -e LETSENCRYPT_EMAIL=admin@synth.guide -e SSL_ACTIVE=1 -e DB_TYPE=postgres -e DB_HOST=db -e DB_PORT=5432 -e DB_PASS_FILE=/etc/wiki/.db-secret -v /etc/wiki/.db-secret:/etc/wiki/.db-secret:ro -e DB_USER=wiki -e DB_NAME=wiki -e UPGRADE_COMPANION=1 --restart=unless-stopped -h wiki --network=wikinet -p 80:3000 -p 443:3443 ghcr.io/colevscode/synth-wiki:latest
 docker create --name=wiki-update-companion -v /var/run/docker.sock:/var/run/docker.sock:ro --restart=unless-stopped -h wiki-update-companion --network=wikinet ghcr.io/colevscode/synth-wiki-update-companion:latest
